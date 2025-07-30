b8b5b63633e5071453ff85328c064c129a076128
SonNguyen <72149376+sonnguyen-dba@users.noreply.github.com>
2025-07-30 14:09:27 +0700

    Update OracleTypeMapper.cpp

--- src/initialload/OracleTypeMapper.cpp
+++ src/initialload/OracleTypeMapper.cpp
@@ -21,13 +21,7 @@ std::string OracleTypeMapper::mapToPostgreSQL(const OracleColumnInfo& colInfo) {
     if (it != simpleTypeMap.end()) {
         return it->second;
     }
-    // ⏱️ TIMESTAMP variants
-    if (dataType.find("timestamp") != std::string::npos) {
-        if (dataType.find("with time zone") != std::string::npos)
-            return "timestamp with time zone";
-        return "timestamp";
-    }
-    
+
     if (dataType == "varchar2" || dataType == "nvarchar2" || dataType == "char") {
         return "varchar(" + std::to_string(colInfo.dataLength) + ")";
     }
